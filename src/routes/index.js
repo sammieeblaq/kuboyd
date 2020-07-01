79bdbac83df252cd2f581a30b573b05d44929185
Samuel Airehrour <odianoseairehrour@gmail.com>
2020-07-01 09:59:27 +0100

    Added the Procfile

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const { auth } = require("../middleware/authorization");
 
-router.use("/", auth, require("./account"));
-router.use("/", auth, require("./transaction"));
-router.use("/", auth, require("./user"));
-// router.use("/", require("./account"));
-// router.use("/", require("./transaction"));
-// router.use("/", require("./user"));
+// router.use("/", auth, require("./account"));
+// router.use("/", auth, require("./transaction"));
+// router.use("/", auth, require("./user"));
+router.use("/", require("./account"));
+router.use("/", require("./transaction"));
+router.use("/", require("./user"));
 router.use("/", require("./auth"));
 
 module.exports = router;
---
+++ src/utils/validate.utils.js
@@ -0,0 +1,4 @@
+// check phone
+// check email
+// check password
+// check account number
