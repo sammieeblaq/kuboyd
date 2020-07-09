8c6f3aa7a677c773aa1e802e742dccc338a5b652
Samuel Airehrour <odianoseairehrour@gmail.com>
2020-07-10 00:47:42 +0100

    Added the mongodb atlas prod file

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,23 @@ const mongoose = require("mongoose");
 
 const connectDb = () => {
   try {
-    mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
-    console.info("connected to the database!!!");
+    if (process.env.NODE_ENV == "prodction") {
+      mongoose.connect(process.env.MONGO_LIVE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      });
+      console.info("connected to the live database!!!");
+    } else {
+      mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      });
+      console.info("connected to the database!!!");
+    }
   } catch (error) {
     throw Error("An error occured here");
   }
