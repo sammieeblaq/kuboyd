c47ad746b1a59a510b8d750eb1d1a64e80cd1fc1
Samuel Airehrour <odianoseairehrour@gmail.com>
2020-07-09 23:01:34 +0100

    chore: added the node engine.

--- src/utils/validate.utils.js
+++ src/utils/validate.utils.js
@@ -2,3 +2,13 @@
 // check email
 // check password
 // check account number
+const Joi = require("@hapi/joi");
+const { body, validationResult } = require("express-validator");
+
+module.exports = {
+  validatePhone: (email = "") => {
+    email = email.trim();
+
+    const isValidEmail = email && validator;
+  },
+};
