535089b3ae4254d6bc6370d9dd6c68a3d45a3321
Samuel Airehrour <odianoseairehrour@gmail.com>
2020-07-14 19:36:08 +0100

    changed the db connection structure

--- config/db.js
+++ config/db.js
@@ -2,23 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDb = () => {
   try {
-    if (process.env.NODE_ENV == "prodction") {
-      mongoose.connect(process.env.MONGO_LIVE, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      });
-      console.info("connected to the live database!!!");
-    } else {
-      mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      });
-      console.info("connected to the database!!!");
-    }
+    mongoose.connect(process.env.MONGO_LIVE || process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
   } catch (error) {
     throw Error("An error occured here");
   }
