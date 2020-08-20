6225eebf97b7d922b14b9804d79ecb72b7b5596a
Samuel Airehrour <odianoseairehrour@gmail.com>
2020-08-20 10:32:49 +0100

    chore: created the app folder to contain importations

---
+++ app/app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const connectDb = require("../config/db");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+// const session = require("express-session");
+connectDb();
+
+app.use(logger("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) =>
+  res.status(200).json({ message: "KUBOYD APP successful" })
+);
+
+const routes = require("../routes");
+// Main function to connect to the database
+app.use("/", routes);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,29 +1,4 @@
-require("dotenv").config();
-const connectDb = require("./config/db");
-// const app = require("./src/app/app");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const logger = require("morgan");
-const helmet = require("helmet");
-const compression = require("compression");
-// const session = require("express-session");
-connectDb();
-
-app.use(logger("dev"));
-app.use(helmet());
-app.use(cors());
-app.use(compression());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/", (req, res) =>
-  res.status(200).json({ message: "KUBOYD APP successful" })
-);
-
-const routes = require("./routes");
-// Main function to connect to the database
-app.use("/", routes);
+const app = require("./app/app");
 
 app.listen(process.env.PORT || process.env.port, (err) => {
   if (err) console.err(err);
