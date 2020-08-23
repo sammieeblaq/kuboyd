7de8e9ba76db6ca51d18cbd67920f8ea56a7d201
Samuel Airehrour <odianoseairehrour@gmail.com>
2020-08-23 12:38:03 +0100

    added the coverage folder to .gitignore

--- __tests__/account.test.js
+++ __tests__/account.test.js
@@ -1,32 +1,33 @@
 const request = require("supertest");
 const app = require("../app/app");
 const db = require("../config/db");
+const { expectCt } = require("helmet");
 
 beforeAll(() => {
   db.connect_test();
 });
 
-describe("Enpoint for posting an account", () => {
-  test("post an account", async (done) => {
-    const response = await request(app).post("/accounts").send({
-      id: 1,
-      phone: "23222344543",
-      email: "sairehrour@gmail.com",
-      accountName: "Samuel",
-      type: "savings",
-      bvn: "223223344432334",
-    });
-    expect(response.statusCode).toBe(200);
-    expect(typeof response.body).toBe("object");
-    expect(response.body).toHaveProperty("status");
-    expect(response.body).toHaveProperty("accountName");
-    expect(response.body).toHaveProperty("balance");
-    expect(response.body).toHaveProperty("accountNumber");
-    done();
-  });
-});
+// describe("Enpoint for posting an account", () => {
+//   test("post an account", async (done) => {
+//     const response = await request(app).post("/accounts").send({
+//       id: 1,
+//       phone: "23222344543",
+//       email: "sairehrour@gmail.com",
+//       accountName: "Samuel",
+//       type: "savings",
+//       bvn: "223223344432334",
+//     });
+//     expect(response.statusCode).toBe(200);
+//     expect(typeof response.body).toBe("object");
+//     expect(response.body).toHaveProperty("status");
+//     expect(response.body).toHaveProperty("accountName");
+//     expect(response.body).toHaveProperty("balance");
+//     expect(response.body).toHaveProperty("accountNumber");
+//     done();
+//   });
+// });
 
-describe("Endpoint for getting an account by account number", () => {
+describe("Endpoint for getting by account number", () => {
   test("get account by account number", async (done) => {
     const response = await request(app).get(`/account?accNumber=${9074840226}`);
     expect(response.statusCode).toBe(200);
@@ -36,6 +37,15 @@ describe("Endpoint for getting an account by account number", () => {
     expect(response.body).toHaveProperty("accountNumber");
     expect(response.body).toHaveProperty("status");
     expect(response.body).toHaveProperty("accountBalance");
+    done();
+  });
+});
+
+describe("Endpoint for gettin all the accounts", () => {
+  test("get all the accounts", async (done) => {
+    const response = await request(app).get("/accounts");
+    expect(typeof response.body).toBe("object");
+    expect(response.statusCode).toBe(200);
     done();
   });
 });
